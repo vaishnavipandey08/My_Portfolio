dec120a138b897375844044c8095685217011dad
Vaishnavi <pandeyvaishnavi0805@gmail.com>
2024-05-30 23:53:35 +0530

    Adding Functionality of Menu Button in Navbar

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 //toggle icon navbar 
+let menuIcon = document.querySelector('#menu-icon') ;
+let navbar = document.querySelector('.navbar') ;
 
+menuIcon.onclick = ()  => {
+    menuIcon.classList.toggle('bx-x') ;
+    navbar.classList.toggle('active') ;
+
+}
 
 
 //Scroll Sections
@@ -12,6 +19,16 @@ window.onscroll = () => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 100 ;
         let height = sec.offsetHeight ;
+        let id = sec.getAttribute('id') ;
+
+        if(top >= offset && top < offset + height) {
+
+            navLinks.forEach(links => {
+                links.classList.remove('active') ;
+                document.querySelector('header nav a[href *= ')
+            });
+
+        }
     }) 
 
     //Sticky Header
